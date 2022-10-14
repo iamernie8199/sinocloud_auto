23f66a75dc1f13ec62929a1ab1a81280430f18f8
iamernie8199 <iamernie8199@gmail.com>
2022-10-14 08:33:45 +0800

    Update 名單版.py

--- 名單版.py
+++ 名單版.py
@@ -7,7 +7,6 @@ from json import load, dumps
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait, Select
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
@@ -24,6 +23,7 @@ def latest_download_file():
         except:
             pass
 
+
 def allclose():
     original_window = driver.current_window_handle
     for window_handle in driver.window_handles:
@@ -34,24 +34,24 @@ def allclose():
 
 
 def upload(path):
-    button = driver.find_element('id', 'ctl00_ContentPlaceHolder1_ucUploadButton2_btnLaunch')
-    ActionChains(driver).click(button).perform()
+    btn = driver.find_element('id', 'ctl00_ContentPlaceHolder1_ucUploadButton2_btnLaunch')
+    ActionChains(driver).click(btn).perform()
 
     original_window = driver.current_window_handle
     for window_handle in driver.window_handles:
         if window_handle != original_window:
             driver.switch_to.window(window_handle)
 
-    button = driver.find_element('id', 'uploader_browse')
-    ActionChains(driver).click(button).perform()
+    btn = driver.find_element('id', 'uploader_browse')
+    ActionChains(driver).click(btn).perform()
 
     app = pywinauto.Desktop()
     dlg = app["開啟"]
     dlg["檔案名稱(&N):):Edit"].SetText(path)
     dlg["開啟(&O)"].click()
 
-    button = driver.find_element('class name', 'plupload_start')
-    ActionChains(driver).click(button).perform()
+    btn = driver.find_element('class name', 'plupload_start')
+    ActionChains(driver).click(btn).perform()
 
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "Util_txtDone"))
@@ -64,21 +64,19 @@ def upload(path):
         EC.presence_of_element_located((By.CLASS_NAME, "alertify-log-success"))
     )
 
-    button = driver.find_element('id', 'ctl00_ContentPlaceHolder1_btnQry')
-    ActionChains(driver).click(button).perform()
+    btn = driver.find_element('id', 'ctl00_ContentPlaceHolder1_btnQry')
+    ActionChains(driver).click(btn).perform()
 
-    button = WebDriverWait(driver, 10).until(
+    btn = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_ucGridView1_gvMain_ctl01_btnExportPdf"))
     )
-    ActionChains(driver).click(button).perform()
+    ActionChains(driver).click(btn).perform()
 
     filepath = latest_download_file()
 
     os.rename(filepath, path.split('\\')[-1].replace('txt', 'pdf'))
     filepath = path.split('\\')[-1].replace('txt', 'pdf')
     shutil.move(filepath, rf'\\10.5.20.211\資產負債管理部\權益證券科\權益證券科-暫定架構\四大流程\交易簿\每日交易明細\方向性名單\{filepath}')
-    os.chdir("..")
-    shutil.rmtree(savepath)	
 
 
 with open('pwd.json', 'r') as f:
@@ -100,14 +98,14 @@ appState = {
     'version': 2
 }
 prefs = {
-    'printing.print_preview_sticky_settings.appState': dumps(appState), 
+    'printing.print_preview_sticky_settings.appState': dumps(appState),
     'savefile.default_directory': PDF_savepath,
     "download.default_directory": savepath,
 }
 options.add_experimental_option('prefs', prefs)
 options.add_argument('--kiosk-printing')
 
-driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options = options)
+driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
 driver.get("http://sinocloud.sph/Login.aspx")
 
 # 登入
@@ -121,7 +119,8 @@ print('log in')
 
 allclose()
 
-driver.get('http://sinocloud.sph/EIP/SSOLauncher.aspx?SSOID=STAKEHOLDER&SSOCaption=金控利害關係人管理&Targeturl=10.240.6.7/SOFun/SSORecvMode2.aspx&TargetFrame=ContPage&TargetPage=Stakeholder/Default.aspx')
+driver.get(
+    'http://sinocloud.sph/EIP/SSOLauncher.aspx?SSOID=STAKEHOLDER&SSOCaption=金控利害關係人管理&Targeturl=10.240.6.7/SOFun/SSORecvMode2.aspx&TargetFrame=ContPage&TargetPage=Stakeholder/Default.aspx')
 element = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "divBody"))
 )
@@ -139,3 +138,6 @@ select = Select(driver.find_element('id', 'ctl00_ContentPlaceHolder1_qryRespecti
 select.select_by_value("0")
 
 upload(r'\\10.5.20.211\資產負債管理部\權益證券科\權益證券科-暫定架構\四大流程\交易簿\每日交易明細\方向性名單\量化批次查詢-證號.txt')
+
+os.chdir("..")
+shutil.rmtree(savepath)
